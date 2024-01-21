81363df055db23c818e82be0caf1ad232de3e2b5
Hakan KARAYILMAZ <hkankyilmazz@gmail.com>
2024-01-21 17:24:52 +0300

    exeption example 1

---
+++ Exceptions/src/Mains/FirstExampleMain.java
@@ -0,0 +1,13 @@
+package Mains;
+
+import Models.FirstExampleModel;
+
+public class FirstExampleMain {
+
+    public static void main(String[] args) {
+
+        FirstExampleModel example = new FirstExampleModel();
+        example.quotient();
+
+    }
+}
---
+++ Exceptions/src/Models/FirstExampleModel.java
@@ -0,0 +1,53 @@
+package Models;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class FirstExampleModel {
+
+
+    private boolean contiuneLoop = true;
+
+
+    public FirstExampleModel() {
+    }
+
+    private int executeQuatient(int numberOne, int numberTwo) throws ArithmeticException {
+
+        return numberOne / numberTwo;
+
+    }
+
+    public void quotient() {
+
+        do {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Plase enter the first Number");
+                int numberOne = scanner.nextInt();
+                System.out.println("Plase enter the Second Number");
+                int numberTwo = scanner.nextInt();
+                int result = executeQuatient(numberOne, numberTwo);
+                System.out.println("Result: " + result);
+                contiuneLoop = false;
+
+            } catch (InputMismatchException e) {
+
+                System.err.printf("Error: %s\n", e);
+                System.out.println("Please enter integer number");
+
+            } catch (ArithmeticException e) {
+
+                System.err.printf("Error: %s\n", e);
+                System.out.println("Please dont enter the 0");
+
+
+            }
+
+        } while (contiuneLoop);
+
+
+    }
+
+
+}
